9d0c9157175e0f66c0076497dce00f177d5934ba
Lorenzo <lrenouvin98@gmail.com>
2021-02-01 17:36:33 +0100

    Scrap for Mudjeans

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-console, no-process-exit */
+const brands = require('./brands.json');
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const mudjeans = require('./sources/mudjeansbrand');
+const adresse = require('./sources/adressebrand');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox (eshop = brands[1].url) {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products = await mudjeans.scrape(eshop);
 
     console.log(products);
     console.log('done');
